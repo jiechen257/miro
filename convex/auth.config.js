f707e7381954a167cd2950e1e6e58b66789b7acb
jiechen <1487503910@qq.com>
2024-07-24 13:14:18 +0800

    chore: update readme

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 const authConfig = {
   providers: [
     {
-      domain: "https://amusing-quetzal-49.clerk.accounts.dev",
+      domain: "https://ample-slug-6.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
